12bedf1fd6e86aebb48f3e5418681f7c9c475a1f
MailaFAP <maila_3@hotmail.com>
2023-08-27 19:48:33 -0300

    terminei de adicionar funções na CLI e ajustei erros na mdLinks

--- src/mdLinks.js
+++ src/mdLinks.js
@@ -1,6 +1,8 @@
-import fs from 'fs/promises';
+import fs from 'fs';
+import fsp from 'fs/promises';
 import chalk from 'chalk';
 import path from 'path';
+import axios from 'axios';
 
 //função que extrai os links do arquivo markdown
 function extraiLinks(texto) {
@@ -12,31 +14,68 @@ function extraiLinks(texto) {
 
 //função que lida com os erros
 function trataErro(erro, mensagemErro) {
-  console.log(erro);
   throw new Error(chalk.red(erro.code, mensagemErro));
 }
 
-//função que analisa a extensão, se for markdown ele lê o arquivo e extrai os links e adiciona a propriedade 
-//file no objeto, se não for markdown, ele retorna uma msg de erro
-function mdLinks(caminhoDoArquivo) {
+function processarArquivo(caminhoDoArquivo) {
   const extensoesPermitidas = ['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'];
   if (extensoesPermitidas.includes(path.extname(caminhoDoArquivo))) {
     const caminhoAbsoluto = path.resolve(caminhoDoArquivo);
-    fs
+    return fsp
       .readFile(caminhoAbsoluto, 'utf-8')
       .then((texto) => {
-        let propriedade = extraiLinks(texto);
-        propriedade.forEach((item) => {
-          item.file = caminhoAbsoluto;
+        let links = extraiLinks(texto);
+        links.forEach((link) => {
+          link.file = caminhoAbsoluto;
         });
-        console.log(JSON.stringify(propriedade));
-        return propriedade;
+        return links;
       })
-      .catch((erroDeLeitura) => console.log(trataErro(erroDeLeitura, 'Houve um problema de leitura')));
+      .catch((erroDeLeitura) => {
+        return trataErro(erroDeLeitura, 'Houve um problema de leitura');
+      });      
   } else {
-    const erroDeExtensao = { code: 404 };
-    return trataErro(erroDeExtensao, 'Este arquivo não contém extensão Markdown');
+    return trataErro({ code: 404 }, 'Este arquivo não contém extensão Markdown');
   }
 }
 
-export default mdLinks//('README.md');
+function validaLinks(links) {
+  const promises = links.map((link) =>
+    axios.get(link.href)
+      .then((response) => {
+        link.status = response.status;
+        link.ok = response.status === 200 ? 'OK' : 'FAIL';
+        return link;
+      })
+      .catch(() => {
+        link.status = 404;
+        link.ok = 'FAIL';
+        return link;
+      })
+  )
+  return Promise.all(promises);
+}
+
+
+function mdLinks(caminhoDoArquivo, options) {
+  if (fs.statSync(caminhoDoArquivo).isFile()) {
+    const lista = processarArquivo(caminhoDoArquivo);
+    if (options.validate === true) {
+      return lista.then((data) => {
+        return validaLinks(data)
+        .then((data) => {
+          return data;
+        })
+      })
+    } else {
+      return processarArquivo(caminhoDoArquivo);
+    }
+
+  } else if (fs.stat(caminhoDoArquivo).isDirectory()) {
+    const arquivos = fs.readdirSync(caminhoDoArquivo);
+    arquivos.forEach((nomeDeArquivo) => {
+      return processarArquivo(`${caminhoDoArquivo}/${nomeDeArquivo}`);
+    });
+  }
+}
+
+export default mdLinks;
